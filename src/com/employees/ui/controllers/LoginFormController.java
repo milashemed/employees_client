b6ebf023163162164d6727561801de2465d14c86
Salim Said Hemed <hemed.dev@gmail.com>
2016-08-13 15:16:33 +0300

    Changes Made on The 13th of August

--- src/com/employees/ui/controllers/LoginFormController.java
+++ src/com/employees/ui/controllers/LoginFormController.java
@@ -15,6 +15,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TitledPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.stage.Stage;
 
 
@@ -40,16 +42,36 @@ public class LoginFormController implements Initializable {
     private void loginHandler(ActionEvent event){
         UiUtils.closeStage(event);
     }
+    @FXML
+    Button btncontact;
+    @FXML
+    Button btnforget;
+    @FXML
+    Button btnsave;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         Image im = new Image(getClass().getResourceAsStream("/com/employees/images/account-login-3x.png"));
         btnlogin.setGraphic(new ImageView(im));
+        btnlogin.setMaxWidth(Double.MAX_VALUE);
+        HBox.setHgrow(btnlogin, Priority.ALWAYS);
+        Image imsettingsbtn = new Image(getClass().getResourceAsStream("/com/employees/images/wrench-3x.png"));
+       btnsave.setGraphic(new ImageView(imsettingsbtn));
+       btnsave.setMaxWidth(Double.MAX_VALUE);
+       HBox.setHgrow(btnsave, Priority.ALWAYS);
         Image imlogin = new Image(getClass().getResourceAsStream("/com/employees/images/lock-locked-3x.png"));
-        Image imsettings = new Image(getClass().getResourceAsStream("/com/employees/images/wrench-3x.png"));
+        Image imsettings = new Image(getClass().getResourceAsStream("/com/employees/images/cog-3x.png"));
         Image imhelp = new Image(getClass().getResourceAsStream("/com/employees/images/question-mark-3x.png"));
+        Image imenvelope = new Image(getClass().getResourceAsStream("/com/employees/images/envelope-closed-3x.png"));
+Image imlink =  new Image(getClass().getResourceAsStream("/com/employees/images/external-link-3x.png"));
         panesettings.setGraphic(new ImageView(imsettings));
         panehelp.setGraphic(new ImageView(imhelp));
         panelogin.setGraphic(new ImageView(imlogin));
+        btncontact.setGraphic(new ImageView(imenvelope));
+        btnforget.setGraphic(new ImageView(imlink));
+        btncontact.setMaxWidth(Double.MAX_VALUE);
+        btnforget.setMaxWidth(Double.MAX_VALUE);
+        HBox.setHgrow(btncontact, Priority.ALWAYS);
+        HBox.setHgrow(btnforget, Priority.ALWAYS);
         // TODO
     }    
     
